1da0bc72c662794c1f55863cadb89afc4c8a1ff6
Alexey Baryzhikov <aleksiarts@gmail.com>
2020-11-05 20:36:54 +0300

    Remove unused utilities

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -1,41 +1,14 @@
 package util;
 
-import io.reactivex.rxjava3.core.*;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableConverter;
+import io.reactivex.rxjava3.core.ObservableTransformer;
+import io.reactivex.rxjava3.core.Single;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 public class Utils {
-    private static final DateTimeFormatter f = DateTimeFormatter.ofPattern("hh:mm:ss.SSS");
-
-    public static <T> void log(String tag, T item) {
-        System.out.printf(
-                "%s [%s] %s %s%n",
-                LocalTime.now().format(f),
-                Thread.currentThread().getName(),
-                tag,
-                item
-        );
-    }
-
-    public static void sleep(long duration) {
-        try {
-            TimeUnit.MILLISECONDS.sleep(duration);
-        } catch (InterruptedException ignored) {
-        }
-    }
-
-    public static int randomInt(int from, int to) {
-        return ThreadLocalRandom.current().nextInt(from, to);
-    }
-
-    public static long randomLong(int from, int to) {
-        return ThreadLocalRandom.current().nextLong(from, to);
-    }
 
     public static Observable<Long> nats(long from) {
         return Observable.generate(
